442671cb7e72f3c9b84b7b7ae77de365c4f8ed67
Sanya Srivastava <purvi.sanya@gmail.com>
2020-06-07 19:41:44 +0400

    sign in/out routing added

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageURL: '',
-      box: {}
+      box: {},
+      route: 'signin'
     }
   }
 
@@ -66,18 +67,24 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
   render () {
     return (
       <div className="App">
-        <Particles className="particles"
-                params={particlesOptions}
-              />
-        <Navigation />
-        <SignIn />
+        <Particles className="particles" params={particlesOptions}/>
+        <Navigation onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'signin' 
+        ? <SignIn onRouteChange={this.onRouteChange} />
+        : <div> 
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/>
+        </div>
+        }
       </div>
     );
   }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SignIn = () => {
+const SignIn = ({onRouteChange}) => {
     return (
         <article className="br3 ba b--black-10 mv4 shadow-5 w-100 w-50-m w-25-1 mw6 center">
             <main className="pa4 black-80">
@@ -17,7 +17,7 @@ const SignIn = () => {
                 </div>
                 </fieldset>
                 <div className="">
-                <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+                <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" onClick = {() => onRouteChange('home')}/>
                 </div>
                 <div className="lh-copy mt3">
                 <a href="#0" className="f6 link dim black db">Register</a>
