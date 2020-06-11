194cf07992c6204bfdff12f407d9d81c95720f58
Sanya Srivastava <purvi.sanya@gmail.com>
2020-06-12 00:51:36 +0400

    connecting front end to backend

--- src/App.js
+++ src/App.js
@@ -39,6 +39,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(console.log());
+  }
+
   calculateFaceLocation = (response) => {
     const clarifaiFace = response.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
