5df20bd595ac9e1c5e94f377e4204c30e804030e
Sanya Srivastava <purvi.sanya@gmail.com>
2020-06-13 20:27:24 +0400

    serve pkg installed

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
     this.state.input)
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://sleepy-falls-42326.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://sleepy-falls-42326.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
